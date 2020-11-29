543cb743653d843840b28f3ab6de2f12a72e1745
Carlos Martinez <carlosmart7104@gmail.com>
2020-11-29 15:44:46 -0600

    chore: se creó la base del proyecto y se probó la animación de blobs con Lottie

---
+++ components/Lottie.vue
@@ -0,0 +1,27 @@
+<template>
+  <div ref="animationContainer"></div>
+</template>
+
+<script>
+import lottie from 'lottie-web'
+export default {
+  props: {
+    animationData: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    this.anim = lottie.loadAnimation({
+      container: this.$refs.animationContainer,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: this.animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMinYMin slice',
+      },
+    })
+  },
+}
+</script>
---
+++ pages/lottie.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <lottie id="shape1" :animation-data="animationData" />
+  </div>
+</template>
+
+<script>
+import Lottie from '@/components/Lottie.vue'
+import animationData from '@/assets/animations/blob-blue.json'
+
+export default {
+  name: 'App',
+  components: {
+    lottie: Lottie,
+  },
+  data() {
+    return {
+      animationData,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#shape1 {
+  display: block;
+  width: 100%;
+}
+</style>
